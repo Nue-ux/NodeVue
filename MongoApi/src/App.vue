36f27657c102a0c265eeb5923b7058782b43988e
Nue-ux <lenindanielsantacruzrodrigez@hotmail.com>
2025-05-01 14:18:52 +0200

    Proyecto final de vue enrutameinto

---
+++ MongoApi/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/hogueras">Hogueras</router-link>
+      <router-link to="/participantes">Participantes</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+nav {
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+}
+</style>
